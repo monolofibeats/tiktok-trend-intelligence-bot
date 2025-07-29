8b559d3ee4c4b62e4a94bae911c4f7df1f298fb6
monolofibeats <mono.lofibeats@gmail.com>
2025-07-29 17:26:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import asyncio
 from scraper.tiktok_scraper import scrape_tiktok_trends
 from db.database import save_to_json
 
 if __name__ == "__main__":
-    trends = scrape_tiktok_trends(limit=100)
+    trends = asyncio.run(scrape_tiktok_trends(limit=100))
     if trends:
         save_to_json(trends)
